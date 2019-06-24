9bb14c3fee54b42072c98cd797a130ba1fbf93ee
moniet <moniet.sawhney@gmail.com>
2019-06-24 10:56:03 +0100

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  var hour = parseInt(time.split(':')[0]);
+  var minute = parseInt(time.split(':')[1][1]);
+
+  if (time.indexOf(':') == -1) {
+    return time;
+  } else {
+    if (hour < 12) {
+      return 'Good Morning';
+    } else if (hour >= 12 && hour <= 17) {
+      return 'Good Afternoon';
+    } else if (hour > 17) {
+      return 'Good Evening';
+    }
+  }
+}
 /* Write your implementation of displayMessage() */
+var h1 = document.querySelector('#greeting');
+
+function displayMessage(message) {
+  h1.innerText = message;
+}
