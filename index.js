7a78c1036e442b3ac01213b5be360720efadccb0
flexkat <catherinemaritz1@gmail.com>
2019-06-24 11:13:08 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let timearray = time.split(":")  
+  let times = parseInt(timearray[0]);
+  if (times > 0 && times < 12) {
+    return "Good Morning";
+  }
+  else if (times > 12 && times < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(args) {
+  greeting = document.getElementById('greeting') 
+  return greeting.innerText= args;
+}
