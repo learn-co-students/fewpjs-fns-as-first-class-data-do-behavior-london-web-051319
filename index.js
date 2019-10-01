b42b219321fcbbd496f8b66558a428799a6e1a8c
JalalMadhaji <jalal.almadhaji90@gmail.com>
2019-10-01 13:39:04 +0300

    complete writing the functions

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let msg = timeString.split(":");
+  if (parseInt(msg[0]) < 12) {
+    return "Good Morning";
+  } else if (parseInt(msg[0]) >= 12 && parseInt(msg[0]) < 17) {
+    return "Good Afternoon";
+  } else if (parseInt(msg[0]) >= 17) {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greetFn) {
+  console.log(greetFn);
+  document.querySelector("h1#greeting").innerHTML = greetFn;
+}
